f4b0ec797568c9558a08d447a75dae05808bb94d
Abdul Jawad Azizi <jawadazizi052@gmail.com>
2024-12-02 15:40:32 +0700

    request done

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use ProtoneMedia\LaravelMixins\Request\ConvertsBase64ToFiles;
+
+class UserRequest extends FormRequest
+{
+    use ConvertsBase64ToFiles; // Library untuk convert base64 menjadi File
+
+    public $validator;
+
+    /**
+     * Setting custom attribute pesan error yang ditampilkan
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'password' => 'Kolom Password',
+        ];
+    }
+
+    /**
+     * Tampilkan pesan error ketika validasi gagal
+     *
+     * @return void
+     */
+    public function failedValidation(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function rules(): array
+    {
+        if ($this->isMethod('post')) {
+            return $this->createRules();
+        }
+        return $this->updateRules();
+    }
+
+    /**
+     * Create rules.
+     *
+     * @return array
+     */
+    private function createRules(): array
+    {
+        return [
+            'name' => 'required|max:100',
+            'photo' => 'nullable|file|image',
+            'email' => 'required|email|unique:m_user',
+            'password' => 'required|min:6',
+            'phone_number' => 'numeric',
+            'm_user_roles_id' => 'required',
+        ];
+    }
+
+    /**
+     * Update rules.
+     *
+     * @return array
+     */
+    private function updateRules(): array
+    {
+        return [
+            'name' => 'required|max:100',
+            'photo' => 'nullable|file|image',
+            'email' => 'required|email|unique:m_user,email,' . $this->id,
+            'password' => 'sometimes|nullable|min:6',
+            'phone_number' => 'numeric',
+            'm_user_roles_id' => 'required',
+        ];
+    }
+
+    /**
+     * inisialisasi key "photo" dengan value base64 sebagai "FILE"
+     */
+    protected function base64FileKeys(): array
+    {
+        return [
+            'photo' => 'foto-user.jpg',
+        ];
+    }
+}
